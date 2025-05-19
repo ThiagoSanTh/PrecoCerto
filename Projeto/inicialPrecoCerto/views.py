83034cb74eaafe7894638422a922d7fca5991bbd
ThiagoSanTh <64171135+ThiagoSanTh@users.noreply.github.com>
2025-05-19 18:09:51 +0000

    Configurações Concluidas

--- Projeto/inicialPrecoCerto/views.py
+++ Projeto/inicialPrecoCerto/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def home(request):
+    produtos = Produto.objects.all()
+    return render(request, 'precocerto/home.html', {'produtos': produtos})
--- Projeto/setup/settings.py
+++ Projeto/setup/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inicialPrecoCerto',
-    'inicialPrecoCerto.apps.InicialprecocertoConfig',
+    
 ]
 
 MIDDLEWARE = [
